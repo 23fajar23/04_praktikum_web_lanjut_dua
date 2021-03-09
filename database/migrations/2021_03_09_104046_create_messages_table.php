8331964cade761e55abb3cd35a6249d0bc9d9a3c
= <=>
2021-03-09 17:48:20 +0700

    Setting model & migration

--- database/migrations/2021_03_09_104046_create_messages_table.php
+++ database/migrations/2021_03_09_104046_create_messages_table.php
@@ -14,7 +14,10 @@ class CreateMessagesTable extends Migration
     public function up()
     {
         Schema::create('messages', function (Blueprint $table) {
-            $table->id();
+            $table->id()->autoIncrement();
+            $table->string('name');
+            $table->string('email');
+            $table->string('message');
             $table->timestamps();
         });
     }
